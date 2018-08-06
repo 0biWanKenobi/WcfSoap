e3f499dabb66c1af946825e8851c905fb46fd74c
Stefano La Terra Inghilterra <stefano.laterrainghilterra@live.com>
2018-08-06 22:14:00 +0200

    Aggiungere i file di progetto.

---
+++ WCFService/App_Code/IService.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+// NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di interfaccia "IService" nel codice e nel file di configurazione contemporaneamente.
+[ServiceContract(Namespace = "http://wcfsoap/services")]
+public interface IService
+{
+
+	// Ensures consistency in the namespace declarations across services
+	[OperationContract]
+	string GetData(int value);
+
+	[OperationContract]
+	CompositeType GetDataUsingDataContract(CompositeType composite);
+
+	// TODO: aggiungere qui le operazioni del servizio
+}
+
+// Per aggiungere tipi compositi alle operazioni del servizio utilizzare un contratto di dati come descritto nell'esempio seguente.
+[DataContract]
+public class CompositeType
+{
+	[DataMember]
+	public bool BoolValue { get; set; }
+
+	[DataMember]
+	public string StringValue { get; set; } = "Hello ";
+}
---
+++ WCFService/App_Code/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di classe "Service" nel codice, nel file svc e nel file di configurazione contemporaneamente.
+public class Service : IService
+{
+	public string GetData(int value)
+	{
+		return $"You entered: {value}";
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException(nameof(composite));
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+}
